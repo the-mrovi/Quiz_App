a18b7403904f3e6a66cdf68771e4eb4d57476fa0
the-mrovi <cse_0182310012101120@lus.ac.bd>
2025-09-27 20:44:52 +0600

    nothing

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -24,7 +25,7 @@ class StartScreen extends StatelessWidget {
 
             Text(
               "Learn Flutter in fun way",
-              style: TextStyle(
+              style: GoogleFonts.tradeWinds(
                 color: const Color.fromARGB(255, 205, 205, 205),
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
